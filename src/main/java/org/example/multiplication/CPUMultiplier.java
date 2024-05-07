d481fe00cc1e6f7443ddb8e7f8051cee08bfef57
VanyaXIII <gurianov.iv@phystech.edu>
2024-05-07 15:47:51 +0300

    CPU multiplication optimized

--- src/main/java/org/example/multiplication/CPUMultiplier.java
+++ src/main/java/org/example/multiplication/CPUMultiplier.java
@@ -13,14 +13,26 @@ public class CPUMultiplier extends Multiplier {
         this.n = n;
     }
 
+    private float[] getTransposedMat() {
+        int k = secondMat.length / n;
+        float[] res = new float[secondMat.length];
+        for (int i = 0; i < k; i++) {
+            for (int j = 0; j < n; j++) {
+                res[j * k + i] = secondMat[i * n + j];
+            }
+        }
+        return res;
+    }
+
     @Override
     public void calculate() {
         int k = firstMat.length / m;
+        float[] transposed = getTransposedMat();
         for (int row = 0; row < m; ++row) {
             for (int col = 0; col < n; ++col) {
                 resultMat[row * n + col] = 0.F;
                 for (int i = 0; i < k; ++i) {
-                    resultMat[row * n + col] += firstMat[row * k + i] * secondMat[i * n + col];
+                    resultMat[row * n + col] += firstMat[row * k + i] * transposed[col * k + i];
                 }
             }
         }
